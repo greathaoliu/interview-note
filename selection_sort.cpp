b586bf962d173aeb960be326c3e9b5bcc426fc19
HaoLiu97 <519555327@qq.com>
2020-08-17 16:27:13 +0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -6,12 +6,12 @@ void SelectionSort(vector<int> &arr) {
     int len = arr.size();
     for(int i = 0; i < len - 1; i ++) {
         int min = i;
-        for (int j = i + 1; j < arr.size(); j ++) {
+        for(int j = i + 1; j < len; j ++) {
             if(arr[j] < arr[min]) {
                 min = j;
             }
-            swap(arr[i], arr[min]);
         }
+        swap(arr[i], arr[min]);
     }
 }
 
