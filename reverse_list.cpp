6645016d50c4690364b1e1f56f95df29c88d9193
HaoLiu97 <519555327@qq.com>
2020-08-19 16:41:12 +0800

    Update reverse_list.cpp

--- reverse_list.cpp
+++ reverse_list.cpp
@@ -9,43 +9,43 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-ListNode* ReverseList(ListNode* head) {
-    if(head == nullptr||head->next == nullptr) return head;
-    ListNode* p = ReverseList(head->next);
+ListNode *ReverseList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) return head;
+    ListNode *p = ReverseList(head->next);
     head->next->next = head;
     head->next = nullptr;
     return p;
 }
 
 // 1->2->3->4->nullptr
-ListNode* ReverseListIteration(ListNode* head) {
-    ListNode* cur = nullptr, *pre = head;
-    while (pre != nullptr) {
-        ListNode* t = pre->next;
-        pre->next = cur;
-        cur = pre;
-        pre = t;
+ListNode *ReverseListIteration(ListNode *head) {
+    ListNode *pre = nullptr, *cur = head;
+    while (cur != nullptr) {
+        ListNode *t = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = t;
     }
-    return cur;
+    return pre;
 }
 
 int main() {
-    ListNode* head = new ListNode(1);
+    ListNode *head = new ListNode(1);
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
 
-    ListNode* p = head;
-    while(p != nullptr) {
-        cout<<p->val<<" ";
+    ListNode *p = head;
+    while (p != nullptr) {
+        cout << p->val << " ";
         p = p->next;
     }
 
-    cout<<endl;
+    cout << endl;
 
     p = ReverseListIteration(head);
-    while(p != nullptr) {
-        cout<<p->val<<" ";
+    while (p != nullptr) {
+        cout << p->val << " ";
         p = p->next;
     }
 }
